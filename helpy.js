bdcfb8e241588a022c588d07cb4433fb11aff5ba
Giovani Henriques <giovanifvhenriques@gmail.com>
2020-05-19 20:05:43 -0300

    Final preview

--- helpy.js
+++ helpy.js
@@ -1,8 +1,16 @@
+<script>
 $(document).ready(function(){
-  var iconfolder = $('.vgg-folder');
-  if (iconfolder.length > 0){
-    iconfolder.each(function(){
-      this;
+  var icons = $('[class*=" glyphicon-gg-"]');
+  if (icons.length > 0){
+    icons.each(function(){
+      this.innerHTML = "<i class=" + this.className.substring(this.className.indexOf("gg-"), this.className.length) + "></i>"
+    });
+  }
+  var iconAPI = $('.glyphicon-gg-arrows-exchange');
+  if (iconAPI.length > 0){
+    iconAPI.each(function(){
+      this.innerHTML;
     });
   }
 });
+</script>
